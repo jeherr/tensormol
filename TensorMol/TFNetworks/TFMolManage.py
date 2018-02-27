bac7cf9af49a3433613f5633de8bcec805ef5095
Kun Yao <kyao@nd.edu>
2018-02-27 15:45:33 -0500

    Channel for SymFunc

--- TensorMol/TFNetworks/TFMolManage.py
+++ TensorMol/TFNetworks/TFMolManage.py
@@ -126,6 +126,8 @@ class TFMolManage(TFManage):
 			self.Instances = MolInstance_DirectBP_EE_SymFunction(self.TData)
 		elif (self.NetType == "fc_sqdiff_BP_Direct_EandG_SymFunction"):
 			self.Instances = MolInstance_DirectBP_EandG_SymFunction(self.TData)
+		elif (self.NetType == "fc_sqdiff_BP_Direct_EandG_SymChannel"):
+			self.Instances = MolInstance_DirectBP_EandG_SymChannel(self.TData)
 		elif (self.NetType == "fc_sqdiff_BP_Direct_Charge_SymFunction"):
 			self.Instances = MolInstance_DirectBP_Charge_SymFunction(self.TData)
 		elif (self.NetType == "Dipole_BP"):
@@ -1485,6 +1487,8 @@ class TFMolManage(TFManage):
 			self.Instances = MolInstance_DirectBP_EE_SymFunction(None,self.TrainedNetworks[0], Trainable_ = self.Trainable)
 		elif (self.NetType == "fc_sqdiff_BP_Direct_EandG_SymFunction"):
 			self.Instances = MolInstance_DirectBP_EandG_SymFunction(None,self.TrainedNetworks[0], Trainable_ = self.Trainable)
+		elif (self.NetType == "fc_sqdiff_BP_Direct_EandG_SymChannel"):
+			self.Instances =MolInstance_DirectBP_EandG_SymChannel(None,self.TrainedNetworks[0], Trainable_ = self.Trainable)
 		elif (self.NetType == "fc_sqdiff_BP_Direct_Charge_SymFunction"):
 			self.Instances = MolInstance_DirectBP_Charge_SymFunction(None,self.TrainedNetworks[0], Trainable_ = self.Trainable)
 		elif (self.NetType == "Dipole_BP"):
