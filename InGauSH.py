050a57f7280d141f869a90df8d2bc4e625ef4568
John Parkhill <john.parkhill@gmail.com>
2018-03-25 12:07:13 -0400

    Added force learning and CanonicalizeGS is unconditionally stable and invariant

--- InGauSH.py
+++ InGauSH.py
@@ -46,46 +46,56 @@ def CanonicalizePCA(dxyzs, pair_mask ,ChiralInv=True):
 def CanonicalizeGS(dxyzs):
 	"""
 	Canonicalize using nearest three atoms and Graham-Schmidt.
+	If there are not three linearly independent atoms within
+	4A, the output will not be rotationally invariant, although
+	The axes will still be as invariant as possible.
 
 	Args:
 		dxyz: a nMol X maxNatom X maxNatom X 3 tensor of atoms. (differenced from center of embedding
 		ie: ... X i X i = (0.,0.,0.))
 	"""
+	# Append orthogonal axes to dxyzs
 	argshape = tf.shape(dxyzs)
-	togather = tf.reshape(dxyzs,(argshape[0]*argshape[1],argshape[1],3))
-	weights = tf.exp(-1.0*tf.norm(dxyzs,axis=-1))
+	defaultAxes = tf.tile(tf.reshape(4.0*tf.eye(3,dtype=tf.float64),(1,1,3,3)),[argshape[0],argshape[1],1,1])
+	dxyzsandDef = tf.concat([dxyzs,defaultAxes],axis=2)
+
+	realdata = tf.reshape(dxyzs,(argshape[0]*argshape[1],argshape[1],3))
+	togather = tf.reshape(dxyzsandDef,(argshape[0]*argshape[1],argshape[1]+3,3))
+	weights = tf.exp(-1.0*tf.norm(dxyzsandDef,axis=-1))
 	maskedDs = tf.where(tf.equal(weights,1.),tf.zeros_like(weights),weights)
+
 	# GS orth the first three vectors.
 	tosort= tf.reshape(maskedDs,(argshape[0]*argshape[1],-1))
-	vals, inds = tf.nn.top_k(maskedDs,k=4)
-	inds = tf.reshape(inds,(argshape[0]*argshape[1],4))
+	vals, inds = tf.nn.top_k(maskedDs,k=3)
+	inds = tf.reshape(inds,(argshape[0]*argshape[1],3))
 	v1i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,:1]],axis=-1)
 	v2i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,1:2]],axis=-1)
 	v3i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,2:3]],axis=-1)
 	v1 = tf.gather_nd(togather,v1i)
-	v1 /= tf.norm(v1,axis=-1,keepdims=True)+1e-36
+	v1 /= tf.clip_by_value(tf.norm(v1,axis=-1,keepdims=True),1e-36,1e36)
 	v2 = tf.gather_nd(togather,v2i)
 	v2 -= tf.einsum('ij,ij->i',v1,v2)[:,tf.newaxis]*v1
-	v2 /= tf.norm(v2,axis=-1,keepdims=True)+1e-36
+	v2 /= tf.clip_by_value(tf.norm(v2,axis=-1,keepdims=True),1e-36,1e36)
 	v3 = tf.gather_nd(togather,v3i)
 	v3 -= tf.einsum('ij,ij->i',v1,v3)[:,tf.newaxis]*v1
 	v3 -= tf.einsum('ij,ij->i',v2,v3)[:,tf.newaxis]*v2
-	v3 /= tf.norm(v3,axis=-1,keepdims=True)+1e-36
+	v3 /= tf.clip_by_value(tf.norm(v3,axis=-1,keepdims=True),1e-36,1e36)
 	vs = tf.concat([v1[:,tf.newaxis,:],v2[:,tf.newaxis,:],v3[:,tf.newaxis,:]],axis=1)
-	tore = tf.einsum('ijk,ilk->ijl',togather,vs)
+
+	tore = tf.einsum('ijk,ilk->ijl',realdata,vs)
 	return tf.reshape(tore,tf.shape(dxyzs))
 
 class InGauShBPNetwork:
 	def __init__(self,aset=None):
 		self.prec = tf.float64
-		self.batch_size = 512
+		self.batch_size = 128
 		self.MaxNAtom = 32
 		self.learning_rate = 0.0005
 		self.AtomCodes = ELEMENTCODES #np.random.random(size=(MAX_ATOMIC_NUMBER,4))
 		self.AtomTypes = [1,6,7,8]
-		self.l_max = 3
+		self.l_max = 4
 		#self.GaussParams = np.array([[0.35, 0.30], [0.70, 0.30], [1.05, 0.30], [1.40, 0.30], [1.75, 0.30], [2.10, 0.30], [2.45, 0.30],[2.80, 0.30], [3.15, 0.30], [3.50, 0.30], [3.85, 0.30], [4.20, 0.30], [4.55, 0.30], [4.90, 0.30]])
-		self.GaussParams = np.array([[0.35, 0.25], [0.70, 0.25], [1.05, 0.25], [1.40, 0.25], [2.10, 0.25], [2.80, 0.25], [3.15, 0.25], [3.50, 0.25], [3.85, 0.25], [4.20, 0.25], [4.90, 0.25], [5.50, 0.25]])
+		self.GaussParams = np.array([[0.35, 0.24], [0.70, 0.24], [1.05, 0.24], [1.40, 0.24], [1.75, 0.24],[2.10, 0.24], [2.80, 0.24], [2.45, 0.24],[3.15, 0.24], [3.50, 0.24], [3.85, 0.24], [4.20, 0.24], [4.90, 0.24], [5.50, 0.24]])
 		self.nrad = len(self.GaussParams)
 		self.nang = (self.l_max+1)**2
 		self.ncodes = self.AtomCodes.shape[-1]
@@ -234,12 +244,12 @@ class InGauShBPNetwork:
 		CODEKERN2 = tf.get_variable(name="CodeKernel2", shape=(nchan,nchan),dtype=self.prec)
 		# combine the weight kernel with the codes.
 		mix1 = tf.matmul(CODES,CODEKERN1) # ncase X ncode
-		embrs = tf.reshape(emb,(ncase,nchan,-1))
+		embrs = tf.reshape(emb,(ncase,-1,nchan))
 		# Ensure any zero cases don't contribute.
 		msk = tf.where(tf.equal(Zrs,0.0),tf.zeros_like(Zrs),tf.ones_like(Zrs))
 		embrs *= msk[:,:,tf.newaxis]
-		weighted = tf.einsum('ijk,ij->ijk',embrs,mix1)
-		weighted2 = tf.einsum('ijk,jl->ilk',weighted,CODEKERN2)
+		weighted = tf.einsum('ikj,ij->ikj',embrs,mix1)
+		weighted2 = tf.einsum('ikj,jl->ikl',weighted,CODEKERN2)
 		# Now pass it through as usual.
 		l0 = tf.reshape(weighted2,(ncase,-1))
 		l0p = tf.concat([l0,CODES],axis=-1)
@@ -263,11 +273,12 @@ class InGauShBPNetwork:
 			print("step: ", "%7d"%step, "  train loss: ", "%.10f"%(float(loss_)))
 			print(self.sess.run([self.gp_tf])[0])
 			feed_dict = self.NextBatch(self.mset)
-			ens = self.sess.run([self.MolEnergies], feed_dict=feed_dict)
+			ens,frcs = self.sess.run([self.MolEnergies,self.MolGrads], feed_dict=feed_dict)
 			for i in range(10):
-				print("Pred, true: ", ens[0][i], feed_dict[self.groundTruthE_pl][i])
-			print("Mean Abs Error: ", np.average(np.abs(ens[0]-feed_dict[self.groundTruthE_pl])))
-			#print("RotGrad:",self.sess.run([self.RotGrad], feed_dict=feed_dict))
+				print("Pred, true: ", ens[i], feed_dict[self.groundTruthE_pl][i])
+			print("Mean Abs Error: ", np.average(np.abs(ens-feed_dict[self.groundTruthE_pl])))
+			if (self.DoRotGrad):
+				print("RotGrad:",self.sess.run([self.RotGrad], feed_dict=feed_dict))
 		return
 
 	def training(self, loss):
@@ -294,15 +305,17 @@ class InGauShBPNetwork:
 		self.atom_codes = tf.Variable(self.AtomCodes,trainable=True)
 		self.gp_tf  = tf.Variable(self.GaussParams,trainable=True, dtype = self.prec)
 
-		if 0:
+		self.DoRotGrad = False
+		if self.DoRotGrad:
 			thetas = tf.acos(2.0*tf.random_uniform([self.batch_size],dtype=tf.float64)-1.0)
 			phis = tf.random_uniform([self.batch_size],dtype=tf.float64)*2*Pi
 			psis = tf.random_uniform([self.batch_size],dtype=tf.float64)*2*Pi
 			matrices = TF_RotationBatch(thetas,phis,psis)
 			self.xyzs_shifted = self.xyzs_pl - self.xyzs_pl[:,0,:][:,tf.newaxis,:]
 			tmpxyzs = tf.einsum('ijk,ikl->ijl',self.xyzs_shifted, matrices)
-
-		self.dxyzs = tf.expand_dims(self.xyzs_pl, axis=2) - tf.expand_dims(self.xyzs_pl, axis=1)
+			self.dxyzs = tf.expand_dims(tmpxyzs, axis=2) - tf.expand_dims(tmpxyzs, axis=1)
+		else:
+			self.dxyzs = tf.expand_dims(self.xyzs_pl, axis=2) - tf.expand_dims(self.xyzs_pl, axis=1)
 		self.z1z2 = tf.cast(tf.expand_dims(self.zs_pl, axis=2) * tf.expand_dims(self.zs_pl, axis=1),tf.float64)
 		self.pair_mask = BuildMultiplicativeZeroMask(self.z1z2)
 		# Canonicalized difference Vectors.
@@ -312,9 +325,12 @@ class InGauShBPNetwork:
 		self.MolEnergies = tf.reduce_sum(self.AtomEnergies,axis=1,keepdims=False)
 
 		# Optional. Verify that the canonicalized differences are invariant.
-		#self.RotGrad = tf.gradients(self.embedded,psis)[0]
+		if self.DoRotGrad:
+			self.RotGrad = tf.gradients(self.embedded,psis)[0]
 
-		self.loss = tf.losses.mean_squared_error(self.MolEnergies, self.groundTruthE_pl)
+		# Add force error?
+		self.MolGrads = tf.gradients(self.MolEnergies,self.xyzs_pl)[0]
+		self.loss = tf.losses.mean_squared_error(self.MolEnergies, self.groundTruthE_pl)+tf.losses.mean_squared_error(self.MolGrads, self.groundTruthG_pl)
 		tf.add_to_collection('losses', self.loss)
 		self.train_op = self.training(self.loss)
 
