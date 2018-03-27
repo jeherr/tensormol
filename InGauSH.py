531d2655834c830b0d2c9016134e56d4582a952b
John Parkhill <johnparkhill@gmail.com>
2018-03-27 18:32:06 -0400

    What's training now

--- InGauSH.py
+++ InGauSH.py
@@ -1,8 +1,9 @@
 from TensorMol import *
 import numpy as np
 
-b = MSet("HNCO_small")
+#b = MSet("HNCO_small")
 #b = MSet("chemspider12_clean_maxatom35")
+b = MSet("chemspider20_1_opt_all")
 b.Load()
 MAX_ATOMIC_NUMBER = 55
 
@@ -75,8 +76,8 @@ def CanonicalizeGS(dxyzs):
 
 #	inds = tf.Print(inds,[togather,maskedDs,inds,vals],"Masked weights inds vals: ", summarize=5)
 
-	inds = tf.reshape(inds,(argshape[0]*argshape[1],4))
-	vals = tf.reshape(vals,(argshape[0]*argshape[1],4))
+	inds = tf.reshape(inds,(argshape[0]*argshape[1],3))
+	vals = tf.reshape(vals,(argshape[0]*argshape[1],3))
 	v1i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,:1]],axis=-1)
 	v2i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,1:2]],axis=-1)
 	v3i = tf.concat([tf.range(argshape[0]*argshape[1])[:,tf.newaxis],inds[:,2:3]],axis=-1)
@@ -111,14 +112,15 @@ def CanonicalizeGS(dxyzs):
 class InGauShBPNetwork:
 	def __init__(self,aset=None):
 		self.prec = tf.float64
-		self.batch_size = 512
+		self.batch_size = 180 # Force learning strongly modulates what you can do.
 		self.MaxNAtom = 32
-		self.learning_rate = 0.0002
+		self.learning_rate = 0.0001
 		self.AtomCodes = ELEMENTCODES #np.random.random(size=(MAX_ATOMIC_NUMBER,4))
 		self.AtomTypes = [1,6,7,8]
 		self.l_max = 3
 		#self.GaussParams = np.array([[0.35, 0.30], [0.70, 0.30], [1.05, 0.30], [1.40, 0.30], [1.75, 0.30], [2.10, 0.30], [2.45, 0.30],[2.80, 0.30], [3.15, 0.30], [3.50, 0.30], [3.85, 0.30], [4.20, 0.30], [4.55, 0.30], [4.90, 0.30]])
-		self.GaussParams = np.array([[0.36, 0.25], [0.70, 0.24], [1.05, 0.24], [1.38, 0.23], [1.70, 0.23],[2.08, 0.23], [2.79, 0.23], [2.42, 0.23],[3.14, 0.23], [3.50, 0.23], [3.85, 0.23], [4.20, 0.23], [4.90, 0.23], [5.50, 0.22], [6.0, 0.22]])
+		#self.GaussParams = np.array([[0.36, 0.25], [0.70, 0.24], [1.05, 0.24], [1.38, 0.23], [1.70, 0.23],[2.08, 0.23], [2.79, 0.23], [2.42, 0.23],[3.14, 0.23], [3.50, 0.23], [3.85, 0.23], [4.20, 0.23], [4.90, 0.23], [5.50, 0.22], [6.0, 0.22]])
+		self.GaussParams = np.array([[0.36, 0.25], [0.70, 0.24], [1.05, 0.24], [1.38, 0.23], [1.70, 0.23],[2.08, 0.23], [2.79, 0.23], [3.14, 0.23], [3.85, 0.23], [4.90, 0.23], [5.50, 0.22]])
 		self.nrad = len(self.GaussParams)
 		self.nang = (self.l_max+1)**2
 		self.ncodes = self.AtomCodes.shape[-1]
@@ -203,7 +205,8 @@ class InGauShBPNetwork:
 		RAD = tf_gauss(dist_tensor, gauss_params)*pair_mask
 		# Perform each of the contractions.
 		SHRAD = tf.einsum('mijk,mijl->mijkl',SH,RAD)
-		CODES = tf.reshape(tf.gather(elecode, Zs, axis=0),(self.batch_size,self.MaxNAtom,-1)) # mol X maxNatom X 4
+		CODES = tf.reshape(tf.gather(elecode, Zs, axis=0),(self.batch_size,self.MaxNAtom,self.ncodes)) # mol X maxNatom X 4
+		#CODES = tf.stop_gradient(CODES) # Can sig. save memory.
 		SHRADCODE = tf.einsum('mijkl,mjn->mikln',SHRAD,CODES)
 		return SHRADCODE
 
@@ -278,7 +281,8 @@ class InGauShBPNetwork:
 		if (step%10==0):
 			self.saver.save(self.sess, './networks/InGauSH',global_step=step)
 			print("step: ", "%7d"%step, "  train loss: ", "%.10f"%(float(loss_)))
-			print(self.sess.run([self.gp_tf])[0])
+			print("Gauss Params: ",self.sess.run([self.gp_tf])[0])
+			print("AtomCodes: ",self.sess.run([self.atom_codes])[0])
 			feed_dict = self.NextBatch(self.mset)
 			ens,frcs,summary = self.sess.run([self.MolEnergies,self.MolGrads,self.summary_op], feed_dict=feed_dict, options=self.options, run_metadata=self.run_metadata)
 			for i in range(10):
@@ -291,10 +295,13 @@ class InGauShBPNetwork:
 		return
 
 	def training(self, loss):
-		tf.summary.scalar(loss.op.name, loss)
-		optimizer = tf.train.AdamOptimizer(self.learning_rate)
+		#optimizer = tf.train.AdamOptimizer(self.learning_rate)
+		#train_op = optimizer.minimize(loss, global_step=global_step)
+		optimizer = tf.train.AdamOptimizer(learning_rate=(self.learning_rate))
+		gvs = optimizer.compute_gradients(loss)
+		capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
 		global_step = tf.Variable(0, name='global_step', trainable=False)
-		train_op = optimizer.minimize(loss, global_step=global_step)
+		train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
 		return train_op
 
 	def Train(self,mxsteps=30000):
@@ -306,7 +313,7 @@ class InGauShBPNetwork:
 	def Prepare(self):
 		tf.reset_default_graph()
 		self.DoRotGrad = False
-		self.DoForceLearning = True
+		self.DoForceLearning = False
 
 		self.xyzs_pl = tf.placeholder(shape = (self.batch_size,self.MaxNAtom,3), dtype = self.prec)
 		self.zs_pl = tf.placeholder(shape = (self.batch_size,self.MaxNAtom,1), dtype = tf.int32)
@@ -315,7 +322,7 @@ class InGauShBPNetwork:
 		self.groundTruthG_pl = tf.placeholder(shape = (self.batch_size,self.MaxNAtom,3), dtype = tf.float64)
 
 		self.atom_codes = tf.Variable(self.AtomCodes,trainable=True)
-		self.gp_tf  = tf.Variable(self.GaussParams,trainable=False, dtype = self.prec)
+		self.gp_tf  = tf.Variable(self.GaussParams,trainable=True, dtype = self.prec)
 
 		if self.DoRotGrad:
 			thetas = tf.acos(2.0*tf.random_uniform([self.batch_size],dtype=tf.float64)-1.0)
@@ -381,6 +388,10 @@ class InGauShBPNetwork:
 			self.options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 			self.run_metadata = tf.RunMetadata()
 			self.writer.add_run_metadata(self.run_metadata, "init", global_step=None)
+		else:
+			self.options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+			self.run_metadata = tf.RunMetadata()
+			self.writer.add_run_metadata(self.run_metadata, "init", global_step=None)
 
 		self.sess.run(self.init)
 		#self.sess.graph.finalize()
@@ -389,8 +400,8 @@ net = InGauShBPNetwork(b)
 net.Train()
 mi = np.random.randint(len(b.mols))
 m = b.mols[mi]
-print m.atoms, m.coords
+print(m.atoms, m.coords)
 EF = net.GetEnergyForceRoutine(m)
-print EF(m.coords)
+print(EF(m.coords))
 Opt = GeomOptimizer(EF)
 Opt.Opt(m)
