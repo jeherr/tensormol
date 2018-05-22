882b5325b0eff68b6a7c4117e9d7a4a53891b7bc
jeherr <johnherr@gmail.com>
2018-05-22 14:28:44 -0400

    merge fix

--- samples/test_jeherr.py
+++ samples/test_jeherr.py
@@ -846,12 +846,8 @@ def minimize_ob():
 # train_energy_symm_func("water_wb97xd_6311gss")
 # train_energy_GauSH("water_wb97xd_6311gss")
 # train_energy_GauSHv2("chemspider12_wb97xd_6311gss_rand")
-<<<<<<< HEAD
 # train_energy_univ("master_jeherr_rand")
 eval_test_set_univ("kaggle_1_opt")
-=======
-train_energy_univ("chemspider20_345_opt_rand")
->>>>>>> 4e460f43f3bbab8c2a68583e4e867d9836a9ddcc
 # test_h2o()
 # evaluate_BPSymFunc("nicotine_vib")
 # water_dimer_plot()
